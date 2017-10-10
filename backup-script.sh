97e99405273572c9935ea13ab06a5fe388a5c08b
Thinlt <trungthin.luong@trueplus.vn>
2017-10-10 17:19:28 +0700

    Update backup-script.sh

--- backup-script.sh
+++ backup-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BACKUP_DIR=/backup/orocrm
+MAX_FILES=30
 
 if [ ! -d $BACKUP_DIR ]; then
   mkdir -p $BACKUP_DIR
@@ -12,8 +13,18 @@ docker commit -p orocrm magestore/orocrm
 ## backup orocrm database
 docker commit -p orodatabase magestore/orocrm_database
 
-DATE=$( date +%Y%m%d_%H%M%S )
+DATE=$( date +%d )
+MONTH=$( date +%Y%m )
+TIME=$( date +%H%M%S )
 
-docker save -o $BACKUP_DIR/magestore_orocrm_$DATE.tar magestore/orocrm
-docker save -o $BACKUP_DIR/magestore_orodatabase_$DATE.tar magestore/orocrm_database
+docker save -o $BACKUP_DIR/magestore_orocrm_$(echo $MONTH).tar magestore/orocrm
+
+FILE_PREFIX="magestore_orodatabase"
+NUM_FILES=$(ls -l $BACKUP_DIR | grep $FILE_PREFIX | wc -l)
+OVER=$(( NUM_FILES - MAX_FILES ))
+if [ $OVER -gt 0 ]; then
+  ls | grep $FILE_PREFIX | sort | head -n $OVER | xargs -I %s rm $BACKUP_DIR/%s
+fi
+
+docker save -o $BACKUP_DIR/"$FILE_PREFIX"_$MONTH"$DATE"_$TIME.tar magestore/orocrm_database
 
